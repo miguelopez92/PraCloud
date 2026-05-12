c726cc72edbffe02a2f600258c82cf6d659851c9
miguelopez92 <98827849+miguelopez92@users.noreply.github.com>
2026-05-11 19:01:05 -0500

    Update Firebase configuration with new credentials

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,8 @@
-export const firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "TU_AUTH_DOMAIN",
-  projectId: "TU_PROJECT_ID",
-  storageBucket: "TU_STORAGE_BUCKET",
-  messagingSenderId: "TU_MESSAGING_SENDER_ID",
-  appId: "TU_APP_ID"
+const firebaseConfig = {
+  apiKey: "AIzaSyA1pqt3k-Y-vhY8FdhWtyV0KQLnkFQNL0I",
+  authDomain: "clase-cloud-7074b.firebaseapp.com",
+  projectId: "clase-cloud-7074b",
+  storageBucket: "clase-cloud-7074b.firebasestorage.app",
+  messagingSenderId: "722168060577",
+  appId: "1:722168060577:web:57905c687078225ec72ee0"
 };
